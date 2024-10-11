03242f6fbc5608ccf8e9b0ccab1716be1a908742
Zalan888 <witmannz@kkszki.hu>
2024-10-11 10:50:13 +0200

    Done

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int[] nums = new int[10];
+
+        public static int[] values()
+        {
+            var rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = rand.Next(101);
+
+            }
+            return nums;
+        }
+
+        public static int Min()
+        {
+            int min = nums[0];
+            foreach (int i in nums)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+            return min;
+        }
+        public static int Max()
+        {
+            int max = nums[0];
+            foreach (int i in nums)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            values();
+            Console.WriteLine(Min());
+            Console.WriteLine(Max());
+        }
+    }
+    }
+
